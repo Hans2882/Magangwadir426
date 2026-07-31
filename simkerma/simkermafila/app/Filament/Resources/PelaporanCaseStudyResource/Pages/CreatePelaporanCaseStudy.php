c77ffed03e617d94f3985b04583467029b6d5f37
Nakita Gayuh Cakrawala <gayuhhsuh@gmail.com>
2026-07-31 16:20:04 +0700

    Fix: Minor Issue

--- simkerma/simkermafila/app/Filament/Resources/PelaporanCaseStudyResource/Pages/CreatePelaporanCaseStudy.php
+++ simkerma/simkermafila/app/Filament/Resources/PelaporanCaseStudyResource/Pages/CreatePelaporanCaseStudy.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreatePelaporanCaseStudy extends CreateRecord
 {
     protected static string $resource = PelaporanCaseStudyResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
--- simkerma/simkermafila/app/Filament/Resources/PelaporanCaseStudyResource/Pages/EditPelaporanCaseStudy.php
+++ simkerma/simkermafila/app/Filament/Resources/PelaporanCaseStudyResource/Pages/EditPelaporanCaseStudy.php
@@ -16,4 +16,9 @@ class EditPelaporanCaseStudy extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
