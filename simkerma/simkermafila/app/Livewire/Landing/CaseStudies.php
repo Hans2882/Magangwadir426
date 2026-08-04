0d7affdf26c368c59c24ae08287de05a631f481e
Nakita Gayuh Cakrawala <gayuhhsuh@gmail.com>
2026-08-04 13:26:19 +0700

    Add: mitra table into landing page

--- simkerma/simkermafila/app/Livewire/Landing/CaseStudies.php
+++ simkerma/simkermafila/app/Livewire/Landing/CaseStudies.php
@@ -4,11 +4,22 @@ namespace App\Livewire\Landing;
 
 use Livewire\Component;
 use App\Models\Kerjasama;
+use App\Models\Mitra;
 use Livewire\Attributes\Layout;
+use Livewire\WithPagination;
 
 #[Layout('components.layouts.landing')]
 class CaseStudies extends Component
 {
+    use WithPagination;
+
+    public $activeTab = 'case-studies';
+    public $search = '';
+
+    public function updatedSearch()
+    {
+        $this->resetPage();
+    }
     public function render()
     {
         // jenis_dokumen_id = 8 (Case Study), status = Selesai
@@ -18,8 +29,16 @@ class CaseStudies extends Component
             ->latest('tanggal_awal')
             ->get();
 
+        $mitras = Mitra::with(['negara', 'kategori'])
+            ->when($this->search, function ($query) {
+                $query->where('nama_mitra', 'like', '%' . $this->search . '%');
+            })
+            ->orderBy('nama_mitra')
+            ->paginate(10);
+
         return view('livewire.landing.case-studies', [
-            'caseStudies' => $caseStudies
+            'caseStudies' => $caseStudies,
+            'mitras' => $mitras
         ]);
     }
 }
