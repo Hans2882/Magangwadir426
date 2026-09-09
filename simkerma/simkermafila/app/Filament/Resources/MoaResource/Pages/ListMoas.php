67839b03a8f2e0e90e196f26996805141d832955
ROMY <romy.tamvan.banget@gmail.com>
2026-09-09 14:57:16 +0700

    Update Filter ke  Kerjasama API

--- simkerma/simkermafila/app/Filament/Resources/MoaResource/Pages/ListMoas.php
+++ simkerma/simkermafila/app/Filament/Resources/MoaResource/Pages/ListMoas.php
@@ -16,6 +16,12 @@ class ListMoas extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            /*
+            |--------------------------------------------------------------------------
+            | API ACTION
+            |--------------------------------------------------------------------------
+            */
+
             Action::make('api')
                 ->label('Lihat API')
                 ->icon('heroicon-o-code-bracket')
@@ -23,23 +29,39 @@ class ListMoas extends ListRecords
                 ->modalSubmitAction(false)
                 ->modalCancelActionLabel('Tutup')
                 ->modalContent(function () {
+
                     $filters = $this->tableFilters ?? [];
 
                     return view('api.kerjasama', [
                         'endpoint' => url('/api/moa'),
+
                         'label' => 'MoA',
+
                         'filters' => array_filter([
-                            'negara_id' => $filters['negara']['value'] ?? null,
-                            'status' => $filters['status']['value'] ?? null,
-                        ], fn ($value) => $value !== null && $value !== ''),
+                            'negara_id' => $filters['negara']['value']
+                                ?? null,
+
+                            'status' => $filters['status']['value']
+                                ?? null,
+                        ], function ($value) {
+                            return $value !== null
+                                && $value !== '';
+                        }),
                     ]);
                 }),
+
+            /*
+            |--------------------------------------------------------------------------
+            | EXPORT EXCEL
+            |--------------------------------------------------------------------------
+            */
 
             Action::make('export')
                 ->label('Export Excel')
                 ->icon('heroicon-o-arrow-down-tray')
                 ->color('success')
                 ->action(function () {
+
                     $query = $this->getFilteredTableQuery()
                         ->with([
                             'mitra',
@@ -47,11 +69,18 @@ class ListMoas extends ListRecords
                             'prodis',
                             'jenisDokumen',
                         ]);
+
                     return Excel::download(
                         new KerjasamaExport($query),
                         'Data_MoA.xlsx'
                     );
                 }),
+
+            /*
+            |--------------------------------------------------------------------------
+            | CREATE
+            |--------------------------------------------------------------------------
+            */
 
             Actions\CreateAction::make()
                 ->label('Tambah MoA')
--- simkerma/simkermafila/app/Filament/Resources/PksSpkResource/Pages/ListPksSpks.php
+++ simkerma/simkermafila/app/Filament/Resources/PksSpkResource/Pages/ListPksSpks.php
@@ -16,6 +16,12 @@ class ListPksSpks extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            /*
+            |--------------------------------------------------------------------------
+            | API ACTION
+            |--------------------------------------------------------------------------
+            */
+
             Action::make('api')
                 ->label('Lihat API')
                 ->icon('heroicon-o-code-bracket')
@@ -23,39 +29,70 @@ class ListPksSpks extends ListRecords
                 ->modalSubmitAction(false)
                 ->modalCancelActionLabel('Tutup')
                 ->modalContent(function () {
+
                     $filters = $this->tableFilters ?? [];
 
                     return view('api.kerjasama', [
                         'endpoint' => url('/api/pks'),
+
                         'label' => 'PKS / SPK',
+
                         'filters' => array_filter([
-                            'jenis_dokumen_id' => $filters['jenis_dokumen_id']['value'] ?? null,
-                            'prodi_id' => $filters['prodis']['values'] ?? [],
-                            'bidang_id' => $filters['bidang']['value'] ?? null,
-                            'status' => $filters['status']['value'] ?? null,
-                        ], fn ($value) => $value !== null && $value !== '' && $value !== []),
+                            'jenis_dokumen_id' =>
+                                $filters['jenis_dokumen_id']['value']
+                                ?? null,
+
+                            'prodi_id' =>
+                                $filters['prodis']['values']
+                                ?? $filters['prodis']['value']
+                                ?? [],
+
+                            'bidang_id' =>
+                                $filters['bidang']['value']
+                                ?? null,
+
+                            'status' =>
+                                $filters['status']['value']
+                                ?? null,
+                        ], function ($value) {
+                            return $value !== null
+                                && $value !== ''
+                                && $value !== [];
+                        }),
                     ]);
                 }),
 
+            /*
+            |--------------------------------------------------------------------------
+            | EXPORT EXCEL
+            |--------------------------------------------------------------------------
+            */
+
             Action::make('export')
-    ->label('Export Excel')
-    ->icon('heroicon-o-arrow-down-tray')
-    ->color('success')
-    ->action(function () {
+                ->label('Export Excel')
+                ->icon('heroicon-o-arrow-down-tray')
+                ->color('success')
+                ->action(function () {
 
-        $query = $this->getFilteredTableQuery()
-            ->with([
-                'mitra',
-                'bidang',
-                'prodis',
-                'jenisDokumen',
-            ]);
+                    $query = $this->getFilteredTableQuery()
+                        ->with([
+                            'mitra',
+                            'bidang',
+                            'prodis',
+                            'jenisDokumen',
+                        ]);
 
-        return Excel::download(
-            new KerjasamaExport($query),
-            'Data_PKS_SPK.xlsx'
-        );
-    }),
+                    return Excel::download(
+                        new KerjasamaExport($query),
+                        'Data_PKS_SPK.xlsx'
+                    );
+                }),
+
+            /*
+            |--------------------------------------------------------------------------
+            | CREATE
+            |--------------------------------------------------------------------------
+            */
 
             Actions\CreateAction::make()
                 ->label('Tambah PKS / SPK')
